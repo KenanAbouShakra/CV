98c92f8cb0c3ebb845879f281fcc9cc49a5aaac2
Kenan Abou Shakra <92676626+KenanAbouShakra@users.noreply.github.com>
2024-06-12 12:43:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.getElementById('show-more-btn').addEventListener('click', function() {
+  var extraJobs = document.querySelector('.extra-jobs');
+  if (extraJobs.style.display === 'none' || extraJobs.style.display === '') {
+    extraJobs.style.display = 'block';
+    this.textContent = 'Vis mindre';
+  } else {
+    extraJobs.style.display = 'none';
+    this.textContent = 'Les mer';
+  }
+});
 
